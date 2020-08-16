b26ffcf06a8a0f58c35fb4c6d6d33b1e6591d98c
Kaylon <kcyoung@tiksdd.com>
2020-08-16 16:40:17 -0500

    Removed unnecessary code and variables.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,12 +8,7 @@ electionData_length = 0
 voteResults = []
 candidates = []
 voteTotals = []
-voteCount_Khan = 0
-voteCount_Correy = 0
-voteCount_Li = 0
-voteCount_Otooley = 0
-counter = 0
-results = []
+
 #Read data from csv file into a reader
 with open(file_path, "r") as csvfile:
     csvreader = csv.DictReader(csvfile, delimiter = ",")
@@ -29,45 +24,16 @@ for index in range(electionData_length):
     if electionData[index]["Candidate"] not in candidates:
         candidates.append(electionData[index]["Candidate"])
 
-print(candidates)
-
 voteTotals = [0]  *  len(candidates)
-results = [0] * len(candidates)
-print(voteTotals)
+voteResults = [0] * len(candidates)
 
 
 for idx, name in enumerate(candidates):
     for index in range(electionData_length):
         if electionData[index]["Candidate"] == name:
             voteTotals[idx] +=1
-    results[idx] = {"Candidate": name, "VoteTototals" : voteTotals[idx]}
+    voteResults[idx] = {"Candidate": name, "VoteTototals" : voteTotals[idx]}
 
 
 print(voteTotals)
-print(results)
-
-# for index in range(electionData_length):
-#     if electionData[index]["Candidate"] == "Khan":
-#     	voteCount_Khan += 1
-
-    # elif electionData[index]["Candidate"] == "Correy":
-    # 	voteCount_Correy += 1
-    # elif electionData[index]["Candidate"] == "Li":
-    #     voteCount_Li += 1
-    # else:
-    # 	voteCount_Otooley += 1
-
-# voteResults = [
-#     {"Candidate": "Khan", "Vote Count": voteCount_Khan },
-#     {"Candidate": "Correy", "Vote Count": voteCount_Correy },
-#     {"Candidate": "Li", "Vote Count": voteCount_Li },
-#     {"Candidate": "O'Tooley", "Vote Count": voteCount_Otooley },
-# ]
-
-
-# print(voteCount_Khan)
-# print(voteCount_Correy)
-# print(voteCount_Li)
-# print(voteCount_Otooley)
-
-# print(voteResults)
+print(voteResults)
