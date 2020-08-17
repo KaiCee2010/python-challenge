0d9e53fdfc0cf4da30a7c8604685f815707518f6
Kaylon <kcyoung@tiksdd.com>
2020-08-16 21:11:00 -0500

    Added comments to code.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,20 +16,30 @@ with open(infile_path, "r") as csvfile:
 #Add data from reader into a dictionary variable
     for row in csvreader:
         electionData.append(row)
+        #gather a unique list of candidates, if candidate not in list add to list
         if row["Candidate"] not in candidates:
             candidates.append(row["Candidate"])
-	
+
+#grab the length of the list	
 electionData_length = len(electionData)
 
+#loop through the candidate names
 for name in candidates:
+    #loop through the election data
     for index in range(electionData_length):
+        #check the name in the candidate list against the election data
+        #if name equals add to the vote total for that name
         if electionData[index]["Candidate"] == name:
             voteTotals += 1
+    #add all values to the vote total list as a dictionary
     voteResults.append({"Candidate": name, "VoteTotals" : voteTotals, "PercentageVotes": round((voteTotals/electionData_length) * 100, 3)})
+    #reset vote totals for next name
     voteTotals = 0
 
+#sort the voteResults list by the vote totals in descending order so highest total is first
 voteResults = sorted(voteResults, key = lambda i: i["VoteTotals"], reverse = True)
 
+#print all the results with the necessary formatting
 dashes = "---------------------------" 
 print("\n")
 print("Election Results")
@@ -42,7 +52,7 @@ print(dashes)
 print(f'Winner: {voteResults[0]["Candidate"]}')
 print(dashes)
 
-
+#write all the results to the file with the necessary formatting
 with open(outfile_path, 'w') as txtfile:
     txtfile.write("\n")
     txtfile.write("Election Results\n")
